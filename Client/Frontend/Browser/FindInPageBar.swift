cdcfd2f16bc98bd50eb54a2d02761fcc25df647e
farhanpatel <farhanpatel.17@gmail.com>
2018-03-20 12:10:57 -0700

    Remove 'SEL' from method names.

--- Client/Frontend/Browser/FindInPageBar.swift
+++ Client/Frontend/Browser/FindInPageBar.swift
@@ -48,7 +48,7 @@ class FindInPageBar: UIView {
 
         set {
             searchText.text = newValue
-            SELdidTextChange(searchText)
+            didTextChange(searchText)
         }
     }
 
@@ -57,7 +57,7 @@ class FindInPageBar: UIView {
 
         backgroundColor = .white
 
-        searchText.addTarget(self, action: #selector(SELdidTextChange), for: .editingChanged)
+        searchText.addTarget(self, action: #selector(didTextChange), for: .editingChanged)
         searchText.textColor = FindInPageUX.SearchTextColor
         searchText.font = FindInPageUX.SearchTextFont
         searchText.autocapitalizationType = .none
@@ -78,14 +78,14 @@ class FindInPageBar: UIView {
         previousButton.setImage(UIImage(named: "find_previous"), for: [])
         previousButton.setTitleColor(FindInPageUX.ButtonColor, for: [])
         previousButton.accessibilityLabel = NSLocalizedString("Previous in-page result", tableName: "FindInPage", comment: "Accessibility label for previous result button in Find in Page Toolbar.")
-        previousButton.addTarget(self, action: #selector(SELdidFindPrevious), for: .touchUpInside)
+        previousButton.addTarget(self, action: #selector(didFindPrevious), for: .touchUpInside)
         previousButton.accessibilityIdentifier = "FindInPage.find_previous"
         addSubview(previousButton)
 
         nextButton.setImage(UIImage(named: "find_next"), for: [])
         nextButton.setTitleColor(FindInPageUX.ButtonColor, for: [])
         nextButton.accessibilityLabel = NSLocalizedString("Next in-page result", tableName: "FindInPage", comment: "Accessibility label for next result button in Find in Page Toolbar.")
-        nextButton.addTarget(self, action: #selector(SELdidFindNext), for: .touchUpInside)
+        nextButton.addTarget(self, action: #selector(didFindNext), for: .touchUpInside)
         nextButton.accessibilityIdentifier = "FindInPage.find_next"
         addSubview(nextButton)
 
@@ -93,7 +93,7 @@ class FindInPageBar: UIView {
         closeButton.setImage(UIImage(named: "find_close"), for: [])
         closeButton.setTitleColor(FindInPageUX.ButtonColor, for: [])
         closeButton.accessibilityLabel = NSLocalizedString("Done", tableName: "FindInPage", comment: "Done button in Find in Page Toolbar.")
-        closeButton.addTarget(self, action: #selector(SELdidPressClose), for: .touchUpInside)
+        closeButton.addTarget(self, action: #selector(didPressClose), for: .touchUpInside)
         closeButton.accessibilityIdentifier = "FindInPage.close"
         addSubview(closeButton)
 
@@ -147,20 +147,20 @@ class FindInPageBar: UIView {
         return super.becomeFirstResponder()
     }
 
-    @objc fileprivate func SELdidFindPrevious(_ sender: UIButton) {
+    @objc fileprivate func didFindPrevious(_ sender: UIButton) {
         delegate?.findInPage(self, didFindPreviousWithText: searchText.text ?? "")
     }
 
-    @objc fileprivate func SELdidFindNext(_ sender: UIButton) {
+    @objc fileprivate func didFindNext(_ sender: UIButton) {
         delegate?.findInPage(self, didFindNextWithText: searchText.text ?? "")
     }
 
-    @objc fileprivate func SELdidTextChange(_ sender: UITextField) {
+    @objc fileprivate func didTextChange(_ sender: UITextField) {
         matchCountView.isHidden = searchText.text?.isEmpty ?? true
         delegate?.findInPage(self, didTextChange: searchText.text ?? "")
     }
 
-    @objc fileprivate func SELdidPressClose(_ sender: UIButton) {
+    @objc fileprivate func didPressClose(_ sender: UIButton) {
         delegate?.findInPageDidPressClose(self)
     }
 }
